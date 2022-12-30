d39c8ab067701c316d8c1b57527d7b222d0c243d
caique-caetano <caiquecaetano07@gmail.com>
2022-12-30 18:28:00 -0300

    Aplicado todas as partes de diretiva, com ngIf, ngClass.

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./listar-pensamento.component.scss']
 })
 export class ListarPensamentoComponent {
+  listaPensamentos = [
+
+        {
+          conteudo: 'Passo informações para o componente filho.',
+          autoria: 'Componente Pai',
+          modelo: 'modelo3'
+        },
+        {
+          conteudo: 'Minha propriedade é decorada com @Input()',
+          autoria: 'Componente Filho',
+          modelo: 'modelo1'
+        },
+        {
+          conteudo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.         Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+          autoria: 'Componente Pai',
+          modelo: 'modelo3'
+        },
+
+  ];
 
 }
